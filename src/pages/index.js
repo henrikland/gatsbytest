ee93580699d3594136d40bc676c1b882e5e42821
Henrik Persson <henrik@ustwo.com>
2020-03-09 14:37:09 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, StaticQuery } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
