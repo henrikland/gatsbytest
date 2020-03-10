c83ccd4260c0dd089a14d912ebf93dbb67b7286a
Henrik Persson <henrik@ustwo.com>
2020-03-10 16:20:47 +0100

    shit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,6 @@ const SecondPage = ({ location }) => (
 		<SEO title="Page two" />
 		<h1>Hi from the second page</h1>
 		<p>Welcome to page 2</p>
-		<p>{location.state.prismic.allActivitys.edges[1].node.title[0].text}</p>
 		<Link to="/">Go back to the homepage</Link>
 	</Layout>
 )
